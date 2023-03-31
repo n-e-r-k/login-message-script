3de53738a90153064c522404aece7aab126760c8
n-e-r-k <jacob.n.france@gmail.com>
2023-03-31 12:04:57 -0500

    Added zsh install script

---
+++ install_zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Copying /login to /home/$USER/.login"
+
+cp -r ./login /home/$USER/.login
+
+echo "Adding `python3 /home/$USER/.login/launch.py` to zshrc"
+
+echo "python3 /home/$USER/.login/launch.py" >> /home/$USER/.zshrc
+
+echo "Finished install, edit /home/$USER/.login/config.py to change configuration for LMS."
